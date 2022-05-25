2a9a656283691f7dec45ddb84bf988745f640bb5
jvtrf <joaovitorfagundes@hotmail.com>
2022-05-25 15:45:37 -0300

    First Commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  sku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: String;
+  active: boolean;
+  unitsInStock: number;
+  dateCreated: Date;
+  lastUpdated: Date;
+}
