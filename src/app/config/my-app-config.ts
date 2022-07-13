f92e56acfbee0d0eacd3d8dfd4f6298a44c76dd9
jvtrf <joaovitorfagundes@hotmail.com>
2022-07-13 16:03:04 -0300

    V3.0 | Add login component + Okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa5rt9ubhaeFgBw85d7',
+    issuer: 'https://dev-56865174.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
